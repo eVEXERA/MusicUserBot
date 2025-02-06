78ffc93fd54b6897755ced4ac78c8cc31bf520e6
Abhishek Thakur <96389440+eVEXERA@users.noreply.github.com>
2025-02-06 19:08:21 +0530

    Create abhi.sh

---
+++ abhi.sh
@@ -0,0 +1,36 @@
+##    Telegram @Itz_Your_4Bhi ##
+##    Copyright ©️ 2025    ##
+
+
+
+#!/bin/bash
+
+echo ">> FETCHING UPSTREAM..."
+git clone https://github.com/eVEXERA/MusicUserBot /MusicUserBot
+
+echo ">> INSTALLING REQUIREMENTS..."
+cd /MusicUserBot
+pip3 install -U -r requirements.txt
+
+echo ">> STARTING MUSIC PLAYER USERBOT..."
+clear
+echo "
+#     #                        
+##   ## #    #  ####  #  ####  
+# # # # #    # #      # #    # 
+#  #  # #    #  ####  # #      
+#     # #    #      # # #      
+#     # #    # #    # # #    # 
+#     #  ####   ####  #  ####  
+                               
+######                                    
+#     # #        ##   #   # ###### #####  
+#     # #       #  #   # #  #      #    # 
+######  #      #    #   #   #####  #    # 
+#       #      ######   #   #      #####  
+#       #      #    #   #   #      #   #  
+#       ###### #    #   #   ###### #    # 
+                                          
+MUSIC PLAYER USERBOT IS SUCCESSFULLY DEPLOYED!
+"
+python3 main.py
